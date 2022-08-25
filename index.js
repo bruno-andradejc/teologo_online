d06737c3fcdb461785b59b2379c439cd2e567572
bruno-andradejc <108308139+bruno-andradejc@users.noreply.github.com>
2022-08-25 07:33:35 -0300

    at

---
+++ index.js
@@ -0,0 +1,10 @@
+let answers = {};
+
+let quadradoUm = document.getElementById('quadrado1'); 
+let quadradoDois = document.getElementById('quadrado2'); 
+let quadradoTres = document.getElementById('quadrado3'); 
+let quadradoQuatro = document.getElementById('quadrado4'); 
+
+function storeAnswer(question_number, event) {
+    if(event.target.type === 'quadrado')
+}
